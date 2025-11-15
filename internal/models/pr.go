860987b7957ff835f4b293b42f41e961fb5c50a4
Timur Gilyazov <progwayoftimur@gmail.com>
2025-11-15 15:58:11 +0300

    added pr models to new functionality (reassign pr's, merge pr's)

--- internal/models/pr.go
+++ internal/models/pr.go
@@ -2,6 +2,11 @@ package models
 
 import "time"
 
+const (
+	StatusOpen   = "OPEN"
+	StatusMerged = "MERGED"
+)
+
 type PullRequest struct {
 	ID                string     `json:"pull_request_id"`
 	Name              string     `json:"pull_request_name"`
@@ -16,4 +21,13 @@ type CreatePRRequest struct {
 	ID       string `json:"pull_request_id" validate:"required"`
 	Name     string `json:"pull_request_name" validate:"required,min=1"`
 	AuthorID string `json:"author_id" validate:"required"`
+}
+
+type ReassignRequest struct {
+	PRID          string `json:"pull_request_id" validate:"required"`
+	OldReviewerID string `json:"old_reviewer_id" validate:"required"`
+}
+
+type MergePRRequest struct {
+	PRID string `json:"pull_request_id" validate:"required"`
 }
